597e978219ec08bb9fda666aa6e7e6167eeee25d
KaanDarcey <kaanhdarcey@gmail.com>
2020-01-20 20:02:41 -0800

    created and styled hero

---
+++ src/comps/Hero/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './hero.scss';
+
+function Hero({title1, title2, title3}){
+    return(
+        <div className='hero'>
+            <div className='heroText'>
+                <h1 className='heroTitle' >{title1},</h1>
+                <h1 className='heroTitle'>{title2}</h1>
+                <h3 className='heroSubTitle'>{title3}</h3>
+                <button className='btn'>View Projects</button>
+                <button className='btn'>Contact Me</button>
+            </div>
+            <img className='heroImg' src={require('../../imgs/Hero.png')} alt='heroImg'/>
+        </div>
+    )
+}
+
+Hero.defaultProps = {
+    title1: 'Hello',
+    title2: "I'm Kaan Darcey",
+    title3: 'Front-End Developer'
+};
+
+
+export default Hero;
---
+++ src/stories/hero.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hero from '../comps/Hero';
+
+export default {
+    title: 'Hero',
+    component: Hero,
+  };
+  
+  export const DefaultHero = () => {
+      return <Hero />
+  }
