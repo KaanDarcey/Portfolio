eea778c7cf182c12db2e73135c93a66e7e25614e
KaanDarcey <kaanhdarcey@gmail.com>
2020-01-28 18:58:10 -0800

    nav comp created

--- src/comps/Nav/index.js
+++ src/comps/Nav/index.js
@@ -4,17 +4,23 @@ import './nav.scss';
 function Nav({title1, title2, title3}){
     return(
     <div className='navbar'>
-        <div className='navItem'>{title1}</div>
-        <div className='navItem'>{title2}</div>
-        <div className='navItem'>{title3}</div>
+        <div className='navItem'>{title1}
+            <div className='actionBar'></div>
+        </div>
+        <div className='navItem'>{title2}
+            <div className='actionBar'></div>
+        </div>
+        <div className='navItem'>{title3}
+            <div className='actionBar'></div>
+        </div>
     </div>
     )
 }
 
 Nav.defaultProps = {
-    title1: 'Projects',
-    title2: 'About',
-    title3: 'Contact'
+    title1: 'PROJECTS',
+    title2: 'ABOUT',
+    title3: 'CONTACT'
 };
 
 export default Nav;
