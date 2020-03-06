28dcb7f2096123339ef77e6e306f1f947599db35
KaanDarcey <kaanhdarcey@gmail.com>
2020-03-06 12:11:07 -0800

    added img files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import Nav from './comps/Nav';
 import Hero from './comps/Hero';
 import Project from './comps/Projects';
 import About from './comps/About';
+import Contact from './comps/Contact';
+import ProjectPage from './comps/Projects/ProjectPage';
 
 function App() {
+
+const [showProject, setShowProject] = useState(false);
+
+var projectPop = null;
+
+if(showProject == false){
+  projectPop = null;
+} else {
+  projectPop = <ProjectPage setShowProject={setShowProject}/>
+}
+
   return (
     <div style={{overflow:'hidden'}}>
       <Nav />
-      <Hero />
-      <Project />
-      <About />
+      <Hero id='hero'/>
+      <Project id='project' setShowProject={setShowProject} />
+      <About id='about'/>
+      <Contact id='contact'/>
+      {projectPop}
     </div>
   );
 }
