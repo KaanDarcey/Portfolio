9149542767b2eb3917d08580013d181845189f7b
KaanDarcey <kaanhdarcey@gmail.com>
2020-03-06 12:12:19 -0800

    updated hero

--- src/comps/Hero/index.js
+++ src/comps/Hero/index.js
@@ -5,7 +5,7 @@ function Hero({title1, title2, title3}){
     return(
         <div className='hero'>
             <div className='heroText'>
-                <h1 className='heroTitle' >{title1},</h1>
+                {/* <h1 className='heroTitle' >{title1},</h1> */}
                 <h1 className='heroTitle'>{title2}</h1>
                 <h3 className='heroSubTitle'>{title3}</h3>
                 <button className='btn'>View Projects</button>
@@ -18,7 +18,7 @@ function Hero({title1, title2, title3}){
 
 Hero.defaultProps = {
     title1: 'Hello',
-    title2: "I'm Kaan Darcey",
+    title2: "Kaan Darcey",
     title3: 'Front-End Developer'
 };
 
