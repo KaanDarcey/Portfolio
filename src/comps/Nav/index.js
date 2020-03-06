5715a5f62908a8ea5fd6a2292e9a5300515e6793
KaanDarcey <kaanhdarcey@gmail.com>
2020-03-06 12:12:32 -0800

    updated nav

--- src/comps/Nav/index.js
+++ src/comps/Nav/index.js
@@ -4,7 +4,7 @@ import './nav.scss';
 function Nav({title1, title2, title3}){
     return(
     <div className='navbar'>
-        <div className='navItem'>{title1}
+        <div  className='navItem'>{title1}
             <div className='actionBar'></div>
         </div>
         <div className='navItem'>{title2}
