35cf0fe4466210cfdb7f32934c64a92381241672
KaanDarcey <kaanhdarcey@gmail.com>
2020-03-06 12:11:28 -0800

    about updated

--- src/comps/About/index.js
+++ src/comps/About/index.js
@@ -8,11 +8,14 @@ function About(){
                 <div className='bio'>
                     <h2>BIO</h2>
                     <p className='bioDescr'>
-                    My name is Kaan Darcey and I'm a Front-End Developer based in Vancouver. 
+                    My name is Kaan Darcey and I'm a Front-End Developer & UI/UX Designer based in Vancouver. <br/><br/>
 
-                    I’m about to graduate from my diploma of digital design and development at BCIT (with distinctions).
-                    I love problem solving and helping people achieve their goals. I'm passionate about my work and have a strong desire to always be growing and learning.
-                    I have made web applications using HTML, CSS, and JavaScript. I also have experience in making mobile applications using React Native for android and IOS.
+I’m about to graduate from my diploma of digital design and development at BCIT (with distinctions).
+I love problem solving and helping people achieve their goals. I'm passionate about my work and have a strong desire to always be growing and learning.<br/><br/>
+
+I have made web applications using HTML, CSS, and JavaScript. I also have experience in making mobile applications using React Native, PHP, and MySQL for android and IOS. Additionally I am currently working on a project using React.js and SASS.<br/><br/>
+
+I have also worked through the design process which involved User Research (including user surveys, identifying target demographics, user personas, and task models), creating Low-Fidelity Prototypes/Wireframes (using InVision and Balsamiq), creating Mock Ups (using Sketch and Figma), creating High-Fidelity Prototypes (with InVision), and User Testing.
                     </p>
                 </div>
                 <div className='skills'>
@@ -42,7 +45,7 @@ function About(){
                     </div>
                 </div>
             </div>
-            <img className='image' src='' />
+            <img className='vector'  src={require('../../imgs/vector.png')} alt='vector'/>
         </div>
     )
 }
